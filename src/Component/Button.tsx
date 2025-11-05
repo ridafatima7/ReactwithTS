b0a013c8d758a65d34f11ddf652a4c4a0f8e6bf0
ridafatima7 <rida.bsse3962@iiu.edu.pk>
2025-11-05 18:23:06 +0500

    5 Nov 2025

--- src/Component/Button.tsx
+++ src/Component/Button.tsx
@@ -1,9 +1,26 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 
-const MyButton : React.FC = () => {
-  return <button></button>
-
-  
+interface myButtonProp{
+    text:string | number | boolean
+    onClick?:()=> void
+}
+interface Book{
+  name:string;
+  price:number;
+}
+const MyButton : React.FC<myButtonProp> = (props) => {
+  const [value,setValue]=useState<number>(1)
+  const [value1,setvalue1]=useState<Book>({
+    name:'Malala',
+    price:23
+  })
+  return (
+    <>
+    <p>{value}</p>
+        <button onClick={()=> ( value +1)}>
+        {props.text}</button>
+    </>
+  )
 }
 
 export default MyButton
